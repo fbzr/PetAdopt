8a2f1e14d05f59367b18667724e1d6ea8b312bbe
fbzr <fabricio.spb@gmail.com>
2020-08-29 11:13:51 -0700

    create/style navbar component and create routes for home and about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PetCardComponent } from './components/pet-card/pet-card.component';
 import { FeaturedPetsComponent } from './components/featured-pets/featured-pets.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     PetCardComponent,
-    FeaturedPetsComponent
+    FeaturedPetsComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
