bd7617b6e685c7609d97894a4b2e193e8926f95e
fbzr <fabricio.spb@gmail.com>
2020-09-01 09:15:25 -0700

    fix PetSetvice isNewRequest method

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -18,11 +18,15 @@ export class PetService {
   private isNewRequest(filters: Object): Boolean {
     const keys = Object.keys(this.filters);
 
+    console.log('checking request');
+    console.log('previous filters ', this.filters);
+    console.log('new filters', filters);
+    console.log('keys', keys);
     if (keys.length !== Object.keys(filters).length) {
       return true;
     }
 
-    for (let key in keys) {
+    for (let key of keys) {
       if (this.filters[key] !== filters[key]) {
         return true;
       }
@@ -49,6 +53,7 @@ export class PetService {
       this.data = this.http.get(reqUrl, {
         headers: { ['Authorization']: `Bearer ${this.token}` },
       });
+      console.log('service data: ', this.data);
       return this.data;
     }
 
